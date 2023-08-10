2a563e49b081870cdc2ef0bb1ae3a7cdd078e33f
Marc Majcher <majcher@gmail.com>
2023-08-10 11:28:48 -0500

    rm

--- server/debug.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from app import app
-from models import db, Hero, Power, HeroPower
-
-if __name__ == '__main__':
-    with app.app_context():
-        import ipdb; ipdb.set_trace()
