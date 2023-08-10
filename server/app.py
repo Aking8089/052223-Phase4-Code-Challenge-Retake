2b2c9aee0743783aadd5baae9f1267949556e4d3
dominick <vannusdominick@gmail.com>
2023-08-10 10:46:27 -0700

    submission for deadline

--- server/app.py
+++ server/app.py
@@ -19,10 +19,72 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
+
 @app.route('/')
-def home():
+def index():
     return '<h1>Code challenge</h1>'
 
+class Vendors(Resource):
+    def get(self):
+        vendors = [vendor.to_dict() for vendor in Vendor.query.all()]
+        return make_response(vendors, 200)
+api.add_resource(Vendors, "/vendors")
+
+class VendorById(Resource):
+    def get(self, id):
+        vendor = Vendor.query.filter_by(id=id).one_or_none()
+        if vendor is None:
+            return make_response({'error': 'Vendor not found'}, 404)
+        vendor_dict = vendor.to_dict()
+        vendor_dict['vendor_sweets'] = [vs.to_dict() for vs in vendor.vendor_sweets]
+        return make_response(vendor_dict, 200)
+api.add_resource(VendorById, "/vendors/<int:id>")
+
+class Sweets(Resource):
+    def get(self):
+        sweets = [sweet.to_dict() for sweet in Sweet.query.all()]
+        return make_response(sweets, 200)
+api.add_resource(Sweets, "/sweets")
+
+class SweetById(Resource):
+    def get(self, id):
+        sweet = Sweet.query.filter_by(id=id).one_or_none()
+        if sweet is None:
+            return make_response({'error': 'Sweet not found'}, 404)
+        return make_response(sweet.to_dict(), 200)
+api.add_resource(SweetById, "/sweets/<int:id>")
+
+class VendorSweets(Resource):
+    def post(self):
+        fields = request.get_json()
+        try:
+            vendor_sweet = VendorSweet(
+                price=fields.get('price'),
+                sweet_id=fields.get('sweet_id'),
+                vendor_id=fields.get('vendor_id'),
+            )
+            db.session.add(vendor_sweet)
+            db.session.flush()
+            vendor_sweet_dict = vendor_sweet.to_dict()
+            sweet_obj = Sweet.query.filter_by(id=vendor_sweet.sweet_id).one()
+            vendor_obj = Vendor.query.filter_by(id=vendor_sweet.vendor_id).one()
+            vendor_sweet_dict['sweet'] = sweet_obj.to_dict()
+            vendor_sweet_dict['vendor'] = vendor_obj.to_dict()
+            db.session.commit()
+
+            return make_response(vendor_sweet_dict, 201)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+
+    def delete(self, id):
+        vendor_sweet = VendorSweet.query.filter_by(id=id).one_or_none()
+        if vendor_sweet is None:
+            return make_response({'error': 'VendorSweet not found'}, 404)
+        db.session.delete(vendor_sweet)
+        db.session.commit()
+        return make_response({}, 204)
+api.add_resource(VendorSweets, "/vendor_sweets", "/vendor_sweets/<int:id>")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
 })
 
 db = SQLAlchemy(metadata=metadata)
@@ -19,21 +19,29 @@ class Sweet(db.Model):
 
     vendor_sweets = db.relationship('VendorSweet', backref='sweet')
     
-    # TODO Add serialization
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+    serialize_rules = ('-vendor_sweets.sweet',)
     
     def __repr__(self):
         return f'<Sweet {self.id}>'
-
 
 class Vendor(db.Model):
     __tablename__ = 'vendors'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-
     vendor_sweets = db.relationship('VendorSweet', backref='vendor')
     
-    # TODO Add serialization
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+    serialize_rules = ('-vendor_sweets.vendor',)
     
     def __repr__(self):
         return f'<Vendor {self.id}>'
@@ -44,11 +52,25 @@ class VendorSweet(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
-
     sweet_id = db.Column(db.Integer, db.ForeignKey('sweets.id'))
     vendor_id = db.Column(db.Integer, db.ForeignKey('vendors.id'))
-    
-    # TODO Add serialization
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'price': self.price,
+            'sweet_id': self.sweet_id,
+            'vendor_id': self.vendor_id
+        }
+    serialize_rules = ('-sweet.vendor_sweets', '-vendor.vendor_sweets')
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if price is None:
+            raise ValueError('Price cannot be None')
+        if price < 0:
+            raise ValueError('Price must be non-negative')
+        return price
     
     def __repr__(self):
         return f'<VendorSweet {self.id}>'
